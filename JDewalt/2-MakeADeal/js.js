ee195c68bc00683d418ff35ad5d66660644b2fee
theNameIsTommy <30668326+theNameIsTommy@users.noreply.github.com>
2018-10-28 10:03:15 +0100

    finalizing MakeADeal

--- JDewalt/2-MakeADeal/js.js
+++ JDewalt/2-MakeADeal/js.js
@@ -1,1 +1,9 @@
 //console.log("helloka")
+var rnd = Math.floor((Math.random() * 3) + 1);
+console.log("Random generated: "+ rnd);
+
+document.getElementsByClassName('cookie').onclick=function() {
+	var link=document.getElementById("href");
+	link=setAttribute("href","choice"+rnd+".html");
+	return false;
+}
